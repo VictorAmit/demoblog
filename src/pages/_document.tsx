f9b5bd94ab59e568f0dcd5e6609142d7b9737c66
Victor Amit <44795913+VictorAmit@users.noreply.github.com>
2021-06-29 18:39:04 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import * as React from 'react'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+export default class extends Document {
+  static async getInitialProps(...args) {
+    const documentProps = await Document.getInitialProps(...args)
+    const {req, renderPage} = args[0]
+    const page = renderPage()
+
+    return {...documentProps, ...page}
   }
 
   render() {
