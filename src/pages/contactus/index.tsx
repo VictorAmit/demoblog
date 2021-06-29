8c562c27a0f4011a40a17c1dcca467d45b7ea7eb
Victor Amit <44795913+VictorAmit@users.noreply.github.com>
2021-06-29 18:50:29 +0530

    Export

--- src/pages/contactus/index.tsx
+++ src/pages/contactus/index.tsx
@@ -5,25 +5,25 @@ const Contact: FunctionComponent = () => {
     return (
 <>
 <Layout>
-<nav class="breadcrumbs is-full-width breadcrumbs-a" id="breadcrumb"><div class="inner ts-contain "><span><a href="../index.html"><span>Home</span></a></span><span class="delim">&raquo;</span><span class="current">Get In Touch</span></div></nav>
-<div class="main ts-contain cf right-sidebar">
+<nav className="breadcrumbs is-full-width breadcrumbs-a" id="breadcrumb"><div className="inner ts-contain "><span><a href="../index.html"><span>Home</span></a></span><span className="delim">&raquo;</span><span className="current">Get In Touch</span></div></nav>
+<div className="main ts-contain cf right-sidebar">
 
-	<div class="ts-row">
-		<div class="col-8 main-content">
+	<div className="ts-row">
+		<div className="col-8 main-content">
 			
 			
-			<div id="post-565" class="post-565 page type-page status-publish">
+			<div id="post-565" className="post-565 page type-page status-publish">
 
 						
-				<header class="post-header">				
+				<header className="post-header">				
 
 								
-					<h1 class="main-heading the-page-heading entry-title">
+					<h1 className="main-heading the-page-heading entry-title">
 						Get In Touch					</h1>
 				</header>
 				
 					
-				<div class="post-content page-content entry-content">				
+				<div className="post-content page-content entry-content">				
 					
 <p>Lorem Ipsum Press is licensed by <a rel="noopener" href="#" target="_blank">Bionetwork Ltd.</a> Our office is located within the company&#8217;s building.</p>
 
@@ -33,7 +33,7 @@ const Contact: FunctionComponent = () => {
 
 
 
-<hr class="wp-block-separator is-style-wide"/>
+<hr className="wp-block-separator is-style-wide"/>
 
 
 
@@ -45,9 +45,9 @@ const Contact: FunctionComponent = () => {
 
 
 
-<div class="wp-block-contact-form-7-contact-form-selector"><div role="form" class="wpcf7" id="wpcf7-f567-p565-o1" lang="en-US" dir="ltr">
-<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
-<form action="https://smartmag.theme-sphere.com/good-news/get-in-touch/#wpcf7-f567-p565-o1" method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
+<div className="wp-block-contact-form-7-contact-form-selector"><div role="form" className="wpcf7" id="wpcf7-f567-p565-o1" lang="en-US" dir="ltr">
+<div className="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+<form action="https://smartmag.theme-sphere.com/good-news/get-in-touch/#wpcf7-f567-p565-o1" method="post" className="wpcf7-form init" novalidate="novalidate" data-status="init">
 <div style={{display: 'none'}}>
 <input type="hidden" name="_wpcf7" value="567" />
 <input type="hidden" name="_wpcf7_version" value="5.4" />
@@ -56,12 +56,12 @@ const Contact: FunctionComponent = () => {
 <input type="hidden" name="_wpcf7_container_post" value="565" />
 <input type="hidden" name="_wpcf7_posted_data_hash" value="" />
 </div>
-<p><span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Your Name" /></span> </p>
-<p><span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Your Email" /></span> </p>
-<p><span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Subject" /></span></p>
-<p><span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Your Message"></textarea></span> </p>
-<p><input type="submit" value="Send Message" class="wpcf7-form-control wpcf7-submit submit-button" /></p>
-<div class="wpcf7-response-output" aria-hidden="true"></div></form></div></div>
+<p><span className="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Your Name" /></span> </p>
+<p><span className="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" className="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="Your Email" /></span> </p>
+<p><span className="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" className="wpcf7-form-control wpcf7-text" aria-invalid="false" placeholder="Subject" /></span></p>
+<p><span className="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" className="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Your Message"></textarea></span> </p>
+<p><input type="submit" value="Send Message" className="wpcf7-form-control wpcf7-submit submit-button" /></p>
+<div className="wpcf7-response-output" aria-hidden="true"></div></form></div></div>
 				</div>
 
 			</div>
@@ -70,14 +70,14 @@ const Contact: FunctionComponent = () => {
 		
 				
 	
-	<aside class="col-4 main-sidebar has-sep" data-sticky="1">
+	<aside className="col-4 main-sidebar has-sep" data-sticky="1">
 	
-			<div class="inner  theiaStickySidebar">
+			<div className="inner  theiaStickySidebar">
 		
 			
-		<div id="smartmag-block-codes-2" class="widget ts-block-widget smartmag-widget-codes">		
-		<div class="block">
-			<div class="a-wrap"> <a href="https://theme-sphere.com/buy/go.php?theme=smartmag" target="_blank">
+		<div id="smartmag-block-codes-2" className="widget ts-block-widget smartmag-widget-codes">		
+		<div className="block">
+			<div className="a-wrap"> <a href="https://theme-sphere.com/buy/go.php?theme=smartmag" target="_blank">
   <img loading="lazy" src="../wp-content/uploads/sites/6/2021/02/GOODNEWS-AD-SP-01-2X.jpg" width="336" height="280" alt="Demo" />
 </a></div>		</div>
 
